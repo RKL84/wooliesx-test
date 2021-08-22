cf487dae4e74615ac6447e61c759c33e1acee3da
Rakesh <rakeshl84@outlook.com>
2021-08-22 16:54:43 +1000

    fix error

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { productService } from '../services';
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const productCollection = await productService.getProducts(req.query.sortOption);
+  const productCollection = await productService.getProducts(req.query.sortOption as string);
   res.status(200).json(productCollection);
 };
